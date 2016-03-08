09d8c25c415ddf578e33ff47d0703d83e6fe6153
feryandi <feryandi.n@gmail.com>
2016-03-08 15:57:30 +0700

    Layouting Button to Landscape Specification

--- app/src/main/java/in/feryand/locationfinder/CameraActivity.java
+++ app/src/main/java/in/feryand/locationfinder/CameraActivity.java
@@ -1,10 +1,13 @@
 package in.feryand.locationfinder;
 
+import android.hardware.camera2.*;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 
 public class CameraActivity extends AppCompatActivity {
