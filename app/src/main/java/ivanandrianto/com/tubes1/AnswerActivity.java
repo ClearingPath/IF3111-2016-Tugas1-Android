5dffb58038674c5cf2d18febf233c20fedccfb18
Ivan Andrianto <andrianto.ivan@yahoo.co.id>
2016-03-24 17:40:04 +0700

    compass, server request-response, answer page

--- app/src/main/java/ivanandrianto/com/tubes1/AnswerActivity.java
+++ app/src/main/java/ivanandrianto/com/tubes1/AnswerActivity.java
@@ -1,27 +1,57 @@
 package ivanandrianto.com.tubes1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class AnswerActivity extends AppCompatActivity {
-    Spinner spinner;
+    private Spinner spinner;
+    private Button btnSubmit;
+    private String selected;
+    private String address;
+    private int port;
+    private String nim;
+    private String latitude;
+    private String longitude;
+    private String token;
+    private String status;
+    private int check=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer);
 
+        address = getResources().getString(R.string.address);
+        port = Integer.parseInt(getResources().getString(R.string.port));
+        nim = getResources().getString(R.string.nim);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        if(bundle!=null) {
+            latitude = bundle.getString("latitude");
+            longitude = bundle.getString("longitude");
+            token = bundle.getString("token");
+        }
+        TextView tv = (TextView)findViewById(R.id.tv);
+        tv.setText(latitude + " " +  longitude + " " +  token);
+
         spinner = (Spinner) findViewById(R.id.spinner1);
-        //spinner.setOnTouchListener(Spinner_OnTouch);
         List<String> locations = new ArrayList<String>();
         locations.add("gku_barat");
         locations.add("gku_timur");
@@ -42,33 +72,60 @@ public class AnswerActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
                 String item = spinner.getSelectedItem().toString();
                 Toast.makeText(AnswerActivity.this, item, Toast.LENGTH_SHORT).show();
+                selected = item;
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parentView) {
                 // your code here
             }
-
         });
 
-        /*spinner.setOnClickListener(new Spinner.OnClickListener() {
+        btnSubmit = (Button)findViewById(R.id.button_submit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(AnswerActivity.this, "Item Clicked", Toast.LENGTH_SHORT).show();
-            }
-        });*/
+                JSONObject json = new JSONObject();
+                try{
+                    json.put("com", "answer");
+                    json.put("nim", "13513039");
+                    json.put("answer", selected);
+                    json.put("latitude", latitude);
+                    json.put("longitude", longitude);
+                    json.put("token", token);
+                    String response = new SocketClient(address,port,json).execute().get();
+                    Toast.makeText(getApplicationContext(), "zzz" + response, Toast.LENGTH_LONG).show();
+                    JSONObject jsonObject = new JSONObject(response);
 
+                    status = jsonObject.optString("status").toString();
+                    token = jsonObject.optString("token").toString();
+                    Intent resultIntent = new Intent();
+                    Bundle extras = new Bundle();
+                    extras.putString("token", token);
+                    if(status.equals("ok")){
+                        latitude = jsonObject.optString("longitude").toString();
+                        longitude = jsonObject.optString("latitude").toString();
+                        extras.putString("latitude", latitude);
+                        extras.putString("longitude", longitude);
+                    } else if(status.equals("wrong_answer")){
+
+                    } else if(status.equals("finish")){
+                        check = 1;
+                    }
+                    resultIntent.putExtras(extras);
+                    setResult(Activity.RESULT_OK, resultIntent);
+                } catch (JSONException e){
+                    e.printStackTrace();
+                } catch (ExecutionException e){
+                    e.printStackTrace();
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                /*Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);*/
+                finish();
+            }
+        });
     }
-
-    /*private View.OnTouchListener Spinner_OnTouch = new View.OnTouchListener() {
-        public boolean onTouch(View v, MotionEvent event) {
-            if (event.getAction() == MotionEvent.ACTION_UP) {
-                String item = spinner.getSelectedItem().toString();
-                Toast.makeText(AnswerActivity.this, item, Toast.LENGTH_SHORT).show();
-            }
-            return true;
-        }
-    };*/
-
-
 }
--- app/src/main/java/ivanandrianto/com/tubes1/MapsActivity.java
+++ app/src/main/java/ivanandrianto/com/tubes1/MapsActivity.java
@@ -8,13 +8,21 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import org.json.JSONException;
@@ -22,19 +30,40 @@ import org.json.JSONObject;
 
 import java.util.concurrent.ExecutionException;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
-    GoogleMap map;
-    String serverResponse;
-    final String address = "167.205.34.132";
-    final int port = 3111;
-    String response = "";
-    double latitude, longitude;
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback,SensorEventListener {
+
+    /* Google Maps */
+    private GoogleMap map;
+    private double latitude, longitude;
+
+    /* Server */
+    private String serverResponse;
+    final private String address = "167.205.34.132";
+    final private int port = 3111;
+    private String response = "";
+    private String token = "";
+    private String status = "";
+    final String nim = "13513039";
+
+    /* Censored */
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private Sensor magnetometer;
+    private boolean accelerometerSet = false;
+    private boolean magnetometerSet = false;
+    private float[] accelerometerValue = new float[3];
+    private float[] magnetometerValue = new float[3];
+    private float curDegree = 0f;
+    private float[] rotationMatrix = new float[9];
+    private float[] orientation = new float[3];
+    private ImageView compass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        /* Button camera */
         ImageButton btnCamera = (ImageButton)findViewById(R.id.button1);
         btnCamera.setImageResource(R.drawable.camera);
         btnCamera.setOnClickListener(new View.OnClickListener() {
@@ -45,29 +74,41 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         });
 
+        /* Button message */
         ImageButton btnMessage = (ImageButton)findViewById(R.id.button2);
         btnMessage.setImageResource(R.drawable.message);
         btnMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), AnswerActivity.class);
-                startActivity(intent);
+                Bundle extras = new Bundle();
+                extras.putString("latitude", String.valueOf(latitude));
+                extras.putString("longitude", String.valueOf(longitude));
+                extras.putString("token", token);
+                intent.putExtras(extras);
+                /*intent.putExtra("latitude", latitude);
+                intent.putExtra("longitude", longitude);
+                intent.putExtra("token", token);*/
+                startActivityForResult(intent, 1);
             }
         });
 
+        /* Get initial latitude and longitude from server */
         JSONObject json = new JSONObject();
         try{
             json.put("com", "req_loc");
             json.put("nim", "13513039");
-
             String response = new SocketClient(address,port,json).execute().get();
             Toast.makeText(getApplicationContext(), "zzz" + response, Toast.LENGTH_LONG).show();
             JSONObject jsonObject = new JSONObject(response);
-            latitude = -6.8914906;
-            longitude = 107.6084704;
+            /*latitude = -6.8914906;
+            longitude = 107.6084704;*/
+
             //sementara dibalik
             latitude = Double.parseDouble(jsonObject.optString("longitude").toString());
             longitude = Double.parseDouble(jsonObject.optString("latitude").toString());
+            token = jsonObject.optString("token").toString();
+            status = jsonObject.optString("status").toString();
         } catch (JSONException e){
             e.printStackTrace();
         } catch (ExecutionException e){
@@ -76,6 +117,13 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             e.printStackTrace();
         }
 
+        /* Sensor */
+        compass = (ImageView)findViewById(R.id.compass_pointer);
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        /* Fragment */
         FragmentManager fmanager = getSupportFragmentManager();
         Fragment fragment = fmanager.findFragmentById(R.id.map);
         SupportMapFragment supportmapfragment = (SupportMapFragment)fragment;
@@ -87,7 +135,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
         LatLng itb = new LatLng(latitude,longitude);
-        map.addMarker(new MarkerOptions().position(itb).title("Marker in Indonesia"));
+        map.addMarker(new MarkerOptions().position(itb).title("Marker in ITB"));
 
         CameraPosition cameraPosition = new CameraPosition.Builder()
                 .target(itb).zoom(15).build();
@@ -95,4 +143,72 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .newCameraPosition(cameraPosition));
 
     }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(this, accelerometer);
+        sensorManager.unregisterListener(this, magnetometer);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event){
+        if(event.sensor == accelerometer){
+            System.arraycopy(event.values, 0, accelerometerValue, 0, event.values.length );
+            accelerometerSet = true;
+        } else if(event.sensor == magnetometer){
+            System.arraycopy(event.values, 0, magnetometerValue, 0, event.values.length );
+            magnetometerSet = true;
+        }
+        if(accelerometerSet && magnetometerSet){
+            SensorManager.getRotationMatrix(rotationMatrix, null, accelerometerValue, magnetometerValue);
+            SensorManager.getOrientation(rotationMatrix,orientation);
+            float radian = orientation[0];
+            float degree = (float)(Math.toDegrees(radian)+360)%360;
+            RotateAnimation ra = new RotateAnimation(
+                    curDegree,
+                    -degree,
+                    Animation.RELATIVE_TO_SELF,
+                    0.5f,
+                    Animation.RELATIVE_TO_SELF,
+                    0.5f
+            );
+            ra.setDuration(250);
+            ra.setFillAfter(true);
+            compass.startAnimation(ra);
+            curDegree = -degree;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy){
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode) {
+            case (1) : {
+                if (resultCode == Activity.RESULT_OK) {
+                    Intent intent = data;
+                    Bundle bundle = intent.getExtras();
+                    if(bundle!=null) {
+                        token = bundle.getString("token");
+                        //latitude = Double.parseDouble(bundle.getString("latitude"));
+                        //longitude = Double.parseDouble(bundle.getString("longitude"));
+                    }
+                }
+                break;
+            }
+        }
+    }
+
 }
