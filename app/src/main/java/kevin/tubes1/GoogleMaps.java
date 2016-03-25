282d350f88fcb8d1ae609f96a4a0476910f7d8de
Kevin Yauris <kyauris@gmail.com>
2016-03-25 16:16:28 +0700

    Creating map and make class for socket handling

--- app/src/main/java/kevin/tubes1/GoogleMaps.java
+++ app/src/main/java/kevin/tubes1/GoogleMaps.java
@@ -1,27 +1,80 @@
 package kevin.tubes1;
 
+import android.content.Intent;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.OrientationEventListener;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class GoogleMaps extends FragmentActivity implements OnMapReadyCallback {
+    static final LatLng defaultLocation = new LatLng(-6.8899f, 107.6100f);
+    private GoogleMap googleMap;
+
+    private ImageView compass;
+    private float compassDeg = 0f;
+    private SensorManager mSensorManager;
+
+    private float mDeviceOrientation;
+    private float phoneOrientation = 0;
+
+    private OrientationEventListener mOrientationEventListener;
+
+    private Message msg = Message.getInstance();
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    RotateAnimation ra = new RotateAnimation(
+            compassDeg,
+            0,
+            Animation.RELATIVE_TO_SELF, 0.5f,
+            Animation.RELATIVE_TO_SELF,
+            0.5f);
 
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTitle("GoogleMap");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        compass = (ImageView) findViewById(R.id.iv_compass);
+
+        OrientationEventListener mOrientationEventListener =
+                new OrientationEventListener(this, SensorManager.SENSOR_DELAY_GAME) {
+                    @Override
+                    public void onOrientationChanged(int orientation){
+                        mDeviceOrientation = orientation;
+                    }
+                };
+
+        if (mOrientationEventListener.canDetectOrientation()) {
+            mOrientationEventListener.enable();
+        }
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
     }
 
 
@@ -34,13 +87,22 @@ public class GoogleMaps extends FragmentActivity implements OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    public void onMapReady(GoogleMap map) {
+        googleMap = map;
+        setUpMap();
+    }
+
+    public void setUpMap(){
+        googleMap.clear();
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, 15.0f));
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+
+        if ( getResources().getConfiguration().orientation == 1 ) {
+            googleMap.setPadding(20, 0, 20, 120);
+        } else {
+            googleMap.setPadding(0, 0, 120, 10);
+        }
     }
 }
---
+++ app/src/main/java/kevin/tubes1/Message.java
@@ -0,0 +1,71 @@
+package kevin.tubes1;
+
+
+import android.app.Application;
+import android.util.Log;
+
+
+/**
+ * Created by kevin on 25/03/2016.
+ */
+public class Message extends Application {
+
+        private SocketHandler sock;
+
+        private String token;
+        private String nim = "13513042";
+        private double lat;
+        private double lng;
+        private boolean started = false;
+
+        private static Message instance = null;
+
+        @Override
+        public void onCreate() {
+            super.onCreate();
+
+            sock = new SocketHandler();
+            Thread netThread = new Thread(sock);
+            netThread.start();
+
+            instance = this;
+        }
+
+        public static Message getInstance() {
+            return instance;
+        }
+
+        public String getToken() {
+            return token;
+        }
+        public void setToken(String token) {
+            this.token = token;
+        }
+
+        public String getNim() {
+            return nim;
+        }
+        public void setNIM(String nim) {
+            this.nim = nim;
+        }
+
+        public double getLat() { return lat; }
+        public double getLng() {
+            return lng;
+        }
+        public void setLatLng(double lat, double lng) {
+            this.lat = lat;
+            this.lng = lng;
+        }
+
+        public boolean getStarted() { return started; }
+        public void setStarted(boolean started) {
+            this.started = started;
+        }
+
+        public SocketHandler getSock() {
+            sock.rc();
+            return sock;
+        }
+}
+
