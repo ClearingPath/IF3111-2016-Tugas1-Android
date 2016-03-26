5d87b99bf7bd16fad0c0ec0fa528e6f7d7092131
icalF <laser.survivor@gmail.com>
2016-03-26 08:01:12 +0700

    socket implementation

--- app/src/main/java/org/informatika/gitlab/icalf/itblocator/AnswerActivity.java
+++ app/src/main/java/org/informatika/gitlab/icalf/itblocator/AnswerActivity.java
@@ -1,13 +1,38 @@
 package org.informatika.gitlab.icalf.itblocator;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Spinner;
 
 public class AnswerActivity extends AppCompatActivity {
+  static final public long NIM = 13513004;
+  static final public String host = "167.205.34.132";
+//  static final public String host = "10.0.2.2";
+  static final public short port = 3111;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_answer);
+  }
+
+  public void submitLocation(View view) {
+    String[] locationList =
+      {"gku_barat", "gku_timur", "intel", "cc_barat", "cc_timur", "dpr", "sunken", "perpustakaan", "pau", "kubus"};
+    Spinner spinnerLocation = (Spinner)findViewById(R.id.spinner_location);
+    String choice = locationList[spinnerLocation.getSelectedItemPosition()];
+
+    // TODO : SEND REQUEST TO SERVER
+
+    Intent returnIntent = new Intent();
+    returnIntent.putExtra("result",
+      "{\"status\":\"ok\",\"nim\":\"13512999\",\"longitude\":6.234123132,\"latitude\":0.1234123412,\"token\":\"21nu2f2n3rh23diefef23hr23ew\"}"
+    );
+    setResult(Activity.RESULT_OK, returnIntent);
+    finish();
   }
 }
--- app/src/main/java/org/informatika/gitlab/icalf/itblocator/MapsActivity.java
+++ app/src/main/java/org/informatika/gitlab/icalf/itblocator/MapsActivity.java
@@ -1,7 +1,9 @@
 package org.informatika.gitlab.icalf.itblocator;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 
@@ -11,11 +13,24 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
   private GoogleMap mMap;
   private UiSettings mUiSettings;
+  private Marker location;
+  private String token;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -23,22 +38,79 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     setContentView(R.layout.activity_maps);
 
     SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-      .findFragmentById(R.id.map);
+                                                          .findFragmentById(R.id.map);
     mapFragment.getMapAsync(this);
+
+    // TODO : MAKE ASYNC TASK FOR NET JOB
+    // <<<<<< BEGIN
+    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+    StrictMode.setThreadPolicy(policy);
+    // >>>>>> END
+  }
+
+  private void refreshLocation(JSONObject response) throws JSONException {
+    double longitude = response.getDouble("longitude");
+    double latitude = response.getDouble("latitude");
+    token = response.getString("token");
+
+    location.setPosition(new LatLng(longitude, latitude));
+    mMap.moveCamera(CameraUpdateFactory.newLatLng(location.getPosition()));
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode == 1) {
+      if (resultCode == Activity.RESULT_OK) {
+        String responseJSON = data.getStringExtra("result");
+
+        try {
+          JSONObject response = (JSONObject) new JSONTokener(responseJSON).nextValue();
+
+          if (response.getString("status").equals("ok")) {
+            refreshLocation(response);
+            // TODO : MAKE TOAST LOCATION CORRECT MOVE TO THE NEXT CHALLENGE
+          } else {          // status = wrong_answer
+            // TODO : MAKE TOAST WRONG ANSWER
+          }
+        } catch (JSONException e) {
+          e.printStackTrace();
+        }
+      }
+    }
   }
 
   @Override
   public void onMapReady(GoogleMap googleMap) {
     mMap = googleMap;
-
     mUiSettings = mMap.getUiSettings();
-
     mUiSettings.setZoomControlsEnabled(true);
     mUiSettings.setCompassEnabled(true);
 
-    LatLng sydney = new LatLng(-6.892134, 107.609959);
-    mMap.addMarker(new MarkerOptions().position(sydney).title("Challenge location"));
-    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    try {
+      Socket sock = new Socket(AnswerActivity.host, AnswerActivity.port);
+      DataOutputStream out = new DataOutputStream(sock.getOutputStream());
+      DataInputStream in = new DataInputStream(sock.getInputStream());
+
+      String request =
+        new JSONObject()
+          .put("com", "req_loq")
+          .put("nim", AnswerActivity.NIM)
+          .toString();
+      System.out.println(request);
+      out.writeUTF(request);
+      String responseJSON = in.readUTF();
+      System.out.println(responseJSON);
+      JSONObject response = (JSONObject) new JSONTokener(responseJSON).nextValue();
+
+      location = mMap.addMarker(new MarkerOptions()
+        .position(new LatLng(0.0, 0.0))
+        .title("Challenge location"));
+      refreshLocation(response);
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
   }
 
   public void callCamera(View view) {
@@ -48,6 +120,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
   public void answerLocation(View view) {
     Intent intent = new Intent(this, AnswerActivity.class);
-    startActivity(intent);
+    intent.putExtra("token", token);
+    startActivityForResult(intent, 1);
   }
 }
