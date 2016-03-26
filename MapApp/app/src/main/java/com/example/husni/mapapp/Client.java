e2e6626314674d32dd55dfa02b185d77683313e3
Husni Munaya <husnimun@gmail.com>
2016-03-26 17:42:56 +0700

    Add JSONSimple library

---
+++ MapApp/app/src/main/java/com/example/husni/mapapp/Client.java
@@ -0,0 +1,29 @@
+package com.example.husni.mapapp;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
+
+public class Client extends AsyncTask<Void, Void, Void> {
+
+    Client() {
+    }
+
+    @Override
+    protected Void doInBackground(Void... arg0) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        super.onPostExecute(result);
+    }
+
+}
--- MapApp/app/src/main/java/com/example/husni/mapapp/SubmitAnswerActivity.java
+++ MapApp/app/src/main/java/com/example/husni/mapapp/SubmitAnswerActivity.java
@@ -11,12 +11,23 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.view.View.OnClickListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 
 public class SubmitAnswerActivity extends AppCompatActivity implements OnItemSelectedListener {
 
     private Spinner locationSpinner;
+    private Button submitButton;
+    private TextView response;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +37,8 @@ public class SubmitAnswerActivity extends AppCompatActivity implements OnItemSel
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.location_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         locationSpinner.setAdapter(adapter);
+
+        response = (TextView) findViewById(R.id.responseTextView);
     }
 
     @Override
@@ -40,12 +53,12 @@ public class SubmitAnswerActivity extends AppCompatActivity implements OnItemSel
     }
 
     public void submitAnswerButton(View view) {
-        Context context = getApplicationContext();
-        String spinnerText = locationSpinner.getSelectedItem().toString();
-        int duration = Toast.LENGTH_LONG;
-
-        Toast toast = Toast.makeText(context, spinnerText, duration);
-        toast.show();
+//        Context context = getApplicationContext();
+//        String spinnerText = locationSpinner.getSelectedItem().toString();
+//        int duration = Toast.LENGTH_LONG;
+//
+//        Toast toast = Toast.makeText(context, spinnerText, duration);
+//        toast.show();
     }
 
 
