6eca2df771377a47b9e448d1f89848b0961517da
Vincent Theophilus Ciputra <vincent_theophilusc@yahoo.com>
2016-03-27 00:12:01 +0700

    added open map, compass, camera, layout

---
+++ GoogleMap/app/src/main/java/com/example/user/googlemap/AnswerActivity.java
@@ -0,0 +1,31 @@
+package com.example.user.googlemap;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class AnswerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        addLocationSpinner();
+    }
+
+    public void addLocationSpinner(){
+        Spinner spinner = (Spinner) findViewById(R.id.locationItem);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.LocationItem, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+}
