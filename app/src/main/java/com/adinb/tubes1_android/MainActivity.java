25a7098a4786e8ec024b362c3c9749aa7b22886d
Adin Baskoro <master.adin@gmail.com>
2016-03-26 12:48:10 +0700

    Almost Done!

--- app/src/main/java/com/adinb/tubes1_android/MainActivity.java
+++ app/src/main/java/com/adinb/tubes1_android/MainActivity.java
@@ -3,6 +3,7 @@ package com.adinb.tubes1_android;
 
 import android.Manifest;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
@@ -15,6 +16,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
@@ -22,7 +24,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
@@ -30,36 +31,173 @@ import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
-
-
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener, SensorEventListener {
 
+    public static final int FINISH = 1;
     private LocationManager locationManager;
-    private LocationListener locationListener;
     private GoogleMap googleMap;
     private Location currentLocation;
-    private String json;
     private String imagePath;
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private LinearLayout lin;
     private ImageView image;
     private float currentDegree = 0f;
     private SensorManager mSensorManager;
+    private Marker target;
+    final String address = "167.205.34.132";
+    final int port = 3111;
+
+    String request = "{\"com\":\"req_loc\",\"nim\":\"13513058\"}";
+    String response;
+
+    Toast toast;
+    JSONObject responseJson;
+    JSONObject answerJson;
+
+    Double longitude;
+    Double lat;
 
     final int LOCATION_REQUEST_CODE = 1;
     final int STORAGE_REQUEST_CODE = 2;
+    final int LOCATION_RESULT = 3;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.adinb.tubes1_android/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.adinb.tubes1_android/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    public class RequestTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Socket socket = null;
+            try {
+                socket = new Socket(address, port);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out.println(request); // Send request
+                response = br.readLine(); // Receive response
+
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+                Log.d("HOST", "UHOST");
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.d("IO", "IOException:" + e.toString());
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Toast
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_LONG;
+            Log.d("RESPONSE", response);
+            if (response != null) {
+                Log.d("Response2", response);
+                toast = Toast.makeText(context, response, duration);
+                toast.show();
+            } else {
+                Log.d("Response2", "NULL");
+                toast = Toast.makeText(context, "NULL", duration);
+                toast.show();
+            }
+
+            if (response != null) {
+                try {
+                    responseJson = new JSONObject(response);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    // TODO Terbalik
+                    lat = responseJson.getDouble("longitude");
+                    longitude = responseJson.getDouble("latitude");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                target = googleMap.addMarker(new MarkerOptions().position(new LatLng(lat, longitude)).title("Next Target"));
+            }
+        }
+    }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
@@ -99,7 +237,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             params.width = ViewGroup.LayoutParams.MATCH_PARENT;
             params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
             lin.setOrientation(LinearLayout.HORIZONTAL);
-            lin.setPadding(0,0,0,16);
+            lin.setPadding(0, 0, 0, 16);
             lin.setLayoutParams(params);
 
             View v1 = findViewById(R.id.v1);
@@ -129,7 +267,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         params.width = ViewGroup.LayoutParams.MATCH_PARENT;
         params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
         lin.setOrientation(LinearLayout.HORIZONTAL);
-        lin.setPadding(0,0,0,16);
+        lin.setPadding(0, 0, 0, 16);
         lin.setLayoutParams(params);
 
         View v1 = findViewById(R.id.v1);
@@ -155,19 +293,22 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public void takePhoto(View view){
+    public void takePhoto(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             File photoFile = null;
-            try{
+            try {
                 photoFile = savePhoto();
-            } catch (IOException e){
-                Log.d("IO",e.toString());
+            } catch (IOException e) {
+                Log.d("IO", e.toString());
             }
 
-            if (photoFile != null){
+            if (photoFile != null) {
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
                 Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
@@ -179,9 +320,61 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-    public void submitAnswer(View view){
-        Intent submitIntent = new Intent(this, SubmitAnswer.class).putExtra(Intent.EXTRA_TEXT, json);
-        startActivity(submitIntent);
+    public void submitAnswer(View view) {
+        Intent submitIntent = new Intent(this, SubmitAnswer.class).putExtra(Intent.EXTRA_TEXT, response);
+        startActivityForResult(submitIntent, LOCATION_RESULT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == LOCATION_RESULT) {
+            String result = data.getStringExtra("json");
+            if (resultCode == FINISH) {
+                String status = null;
+                try {
+                    status = new JSONObject(result).getString("status");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                if (status.equals("finish")){
+                    target.remove();
+                    Toast toast2 = Toast.makeText(getApplicationContext(), "FINISH!", Toast.LENGTH_LONG);
+                    toast2.show();
+                    setTitle("FINISH!");
+                }
+                else if (status.equals("ok")){
+                    Toast toast2 = Toast.makeText(getApplicationContext(),"Good Job! Continue with next target.", Toast.LENGTH_LONG);
+                    toast2.show();
+
+                    try {
+                        responseJson = new JSONObject(result);
+                        target.setPosition(new LatLng(responseJson.getDouble("latitude"), responseJson.getDouble("longitude")));
+                        answerJson = new JSONObject();
+
+                        // TODO terbalik
+                        answerJson.put("longitude", responseJson.getDouble("latitude"));
+                        answerJson.put("latitude", responseJson.getDouble("longitude"));
+                        answerJson.put("token", responseJson.getString("token"));
+
+                        response = answerJson.toString();
+                        Log.d("RESPONSE", response);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else if (status.equals("wrong_answer")){
+                    Toast toast2 = Toast.makeText(getApplicationContext(),"Sorry, wrong answer.", Toast.LENGTH_LONG);
+                    toast2.show();
+                }
+                else if (status.equals("back")){
+                    Toast toast2 = Toast.makeText(getApplicationContext(),"Why going back?", Toast.LENGTH_LONG);
+                    toast2.show();
+                }
+
+            } else {
+            }
+        }
     }
 
     private File savePhoto() throws IOException {
@@ -226,8 +419,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         } else {
             // Ask for permission
             ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                LOCATION_REQUEST_CODE);
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_REQUEST_CODE);
         }
 
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
@@ -249,6 +442,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             onLocationChanged(currentLocation);
         }
         locationManager.requestLocationUpdates(bestProvider, 20000, 0, this);
+        new RequestTask().execute(); // First request
 
     }
 
@@ -270,11 +464,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 }
             }
 
-            } else {
-                // Permission was denied. Display an error message.
-            }
-
+        } else {
+            // Permission was denied. Display an error message.
         }
+
+    }
 
     @Override
     public void onLocationChanged(Location location) {
@@ -310,7 +504,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
         mSensorManager.unregisterListener(this);
     }
--- app/src/main/java/com/adinb/tubes1_android/SubmitAnswer.java
+++ app/src/main/java/com/adinb/tubes1_android/SubmitAnswer.java
@@ -1,15 +1,93 @@
 package com.adinb.tubes1_android;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class SubmitAnswer extends AppCompatActivity {
+
+    final String address = "167.205.34.132";
+    final int port = 3111;
+    JSONObject json;
+    JSONObject oldJson;
+    String jsonRequest = "";
+    String response = "";
+    Button submitButton;
+    Spinner locationSpinner;
+
+    public class RequestTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Socket socket = null;
+            try {
+                socket = new Socket (address,port);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+                BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out.println(jsonRequest); // Send request
+                response = br.readLine(); // Receive response
+
+            }
+            catch (UnknownHostException e) {
+                e.printStackTrace();
+                Log.d("HOST", "UHOST");
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.d("IO", "IOException:" + e.toString());
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            Toast toast = Toast.makeText(getApplicationContext(), response,Toast.LENGTH_LONG);
+            toast.show();
+            Intent returnIntent = new Intent();
+
+            returnIntent.putExtra("json", response);
+            setResult(MainActivity.FINISH, returnIntent);
+            finish();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent mIntent = new Intent();
+        mIntent.putExtra("json", "{\"status\":\"back\"}");
+        setResult(MainActivity.FINISH, mIntent);
+        super.onBackPressed();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +96,42 @@ public class SubmitAnswer extends AppCompatActivity {
         setTitle(R.string.title_activity_submit_answer);
 
         // Set Spinner
-        Spinner locationSpinner = (Spinner) findViewById(R.id.spinner);
+        locationSpinner = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.location_array, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<CharSequence> codeadapter = ArrayAdapter.createFromResource(this, R.array.location_code_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         locationSpinner.setAdapter(adapter);
 
+        // Get intent
+        Log.d("INTENT", getIntent().getExtras().getString(Intent.EXTRA_TEXT));
+        try {
+            oldJson = new JSONObject(getIntent().getExtras().getString(Intent.EXTRA_TEXT));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // Get button
+        submitButton = (Button) findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                json = new JSONObject();
+                try {
+                    json.put("com", "answer");
+                    json.put("nim", "13513058");
+                    json.put("answer", codeadapter.getItem(locationSpinner.getSelectedItemPosition()).toString());
+                    // TODO terbalik
+                    json.put("longitude", oldJson.getDouble("latitude"));
+                    json.put("latitude", oldJson.getDouble("longitude"));
+                    json.put("token", oldJson.getString("token"));
+                    jsonRequest = json.toString();
+                    Log.d("JSONTOSTRING", jsonRequest);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                new SubmitAnswer.RequestTask().execute();
+            }
+        });
     }
 
 }
