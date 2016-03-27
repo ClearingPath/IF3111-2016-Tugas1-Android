159cf720c7e37f9fd67064f443aeea42dfe6be96
Vincent Theophilus Ciputra <vincent_theophilusc@yahoo.com>
2016-03-27 21:01:35 +0700

    added json for server, save log to file

--- GoogleMap/app/src/main/java/com/example/user/googlemap/AnswerActivity.java
+++ GoogleMap/app/src/main/java/com/example/user/googlemap/AnswerActivity.java
@@ -1,12 +1,49 @@
 package com.example.user.googlemap;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Calendar;
 
 public class AnswerActivity extends AppCompatActivity {
+
+    Button buttonSubmit;
+    Spinner locationAnswer;
+    private String answer;
+    Socket socket = null;
+    private final String ServerIP = "167.205.34.132";
+    private final int ServerPort = 3111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +53,17 @@ public class AnswerActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         addLocationSpinner();
+
+        locationAnswer = (Spinner)findViewById(R.id.locationItem);
+        buttonSubmit = (Button)findViewById(R.id.buttonSubmit);
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                answer = String.valueOf(locationAnswer.getSelectedItem());
+                new Submit().execute();
+            }
+        });
     }
 
     public void addLocationSpinner(){
@@ -27,5 +75,190 @@ public class AnswerActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+    }
+
+    public class Submit extends AsyncTask<Void, Void, String> {
+        private String status, token;
+        private double latitude, longitude;
+
+        @Override
+        protected String doInBackground(Void... args) {
+
+            try {
+                socket = new Socket(ServerIP, ServerPort);
+                try {
+                    Looper.prepare();
+                    LocationManager lm = (LocationManager) getSystemService(getApplicationContext().LOCATION_SERVICE);
+                    LocationListener ll = new LocationListener() {
+                        @Override
+                        public void onLocationChanged(Location location) {
+                        }
+
+                        @Override
+                        public void onStatusChanged(String provider, int status, Bundle extras) {
+                        }
+
+                        @Override
+                        public void onProviderEnabled(String provider) {
+                        }
+
+                        @Override
+                        public void onProviderDisabled(String provider) {
+                        }
+                    };
+                    lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, ll);
+                    Location location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    if (location != null) {
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
+                    }
+                } catch (SecurityException e) {
+                    e.printStackTrace();
+                }
+
+                SharedPreferences sp = getSharedPreferences("PBD", Activity.MODE_PRIVATE);
+                JSONObject json = new JSONObject();
+                json.put("answer", answer);
+                json.put("com", "answer");
+                json.put("nim", "13513005");
+                json.put("token", sp.getString("token", null));
+                json.put("longitude", longitude);
+                json.put("latitude", latitude);
+
+                String date = java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
+                String log = readFromFile();
+                writeToFile(log + "\nClient:\n" + json + "\n" + date + "\n");
+
+                PrintWriter out = new PrintWriter(socket.getOutputStream());
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                out.println(json.toString());
+                out.flush();
+                System.err.println(json);
+
+                String res = in.readLine();
+                System.err.println(res);
+                return res;
+
+            } catch (UnknownHostException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally{
+                if(socket != null){
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String response) {
+            try {
+                (Toast.makeText(getApplicationContext(),response,Toast.LENGTH_LONG)).show();
+
+                SharedPreferences sp = getSharedPreferences("PBD", Activity.MODE_PRIVATE);
+                SharedPreferences.Editor edit = sp.edit();
+
+                String date = java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
+                String log = readFromFile();
+                writeToFile(log + "\nServer:\n" + response + "\n" + date + "\n");
+
+                JSONObject jsonResponse = new JSONObject(response);
+                status = jsonResponse.getString("status");
+                if (status.equals("ok")){
+                    longitude = jsonResponse.getDouble("longitude");
+                    latitude = jsonResponse.getDouble("latitude");
+                    edit.putString("longitude", Double.toString(longitude));
+                    edit.putString("latitude", Double.toString(latitude));
+                    Toast.makeText(getApplicationContext(), "Right Answer", Toast.LENGTH_LONG).show();
+                }
+                else if (status.equals("wrong_answer")){
+                    Toast.makeText(getApplicationContext(), "Wrong Answer", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Finish", Toast.LENGTH_LONG).show();
+                }
+                token = jsonResponse.getString("token");
+
+                edit.putString("token", token);
+                edit.commit();
+                Intent intent = new Intent();
+                intent.putExtra("status", status);
+                setResult(RESULT_OK, intent);
+                finish();
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    public void writeToFile(String data)
+    {
+        String path = Environment.getExternalStorageDirectory() + File.separator  + "PBD";
+        // Create the folder.
+        File folder = new File(path);
+        folder.mkdirs();
+
+        // Create the file.
+        File file = new File(folder, "log.txt");
+
+        // Save your stream, don't forget to flush() it before closing it.
+
+        try
+        {
+            file.createNewFile();
+            FileOutputStream fOut = new FileOutputStream(file);
+            OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
+            myOutWriter.append(data);
+
+            myOutWriter.close();
+
+            fOut.flush();
+            fOut.close();
+        }
+        catch (IOException e)
+        {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+    private String readFromFile() {
+
+        String ret = "";
+
+        String path = Environment.getExternalStorageDirectory() + File.separator  + "PBD";
+
+        //Get the text file
+        File file = new File(path,"log.txt");
+
+        //Read text from file
+        StringBuilder text = new StringBuilder();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
+            br.close();
+        }
+        catch (IOException e) {
+            //You'll need to add proper error handling here
+        }
+
+        ret = text.toString();
+        return ret;
     }
 }
--- GoogleMap/app/src/main/java/com/example/user/googlemap/MapsActivity.java
+++ GoogleMap/app/src/main/java/com/example/user/googlemap/MapsActivity.java
@@ -1,7 +1,10 @@
 package com.example.user.googlemap;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -11,24 +14,48 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Bundle;
+import android.os.AsyncTask;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener, SensorEventListener {
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    private GoogleMap mMap;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Calendar;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener, SensorEventListener {
+
+    private GoogleMap mMap; 
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
 
     private ImageView arrowMap;
@@ -42,6 +69,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private float[] mR = new float[9];
     private float[] mOrientation = new float[3];
     private float mCurrentDegree = 0f;
+
+    Socket socket = null;
+    private final String ServerIP = "167.205.34.132";
+    private final int ServerPort = 3111;
+    private String token, status;
+    private double longitude, latitude;
+    private boolean firstConnect = true;
+    Marker answerLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,8 +92,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
         arrowMap = (ImageView) findViewById(R.id.arrowMap);
-    }
 
+        if(firstConnect == true)
+            new connection().execute();
+        else {
+            SharedPreferences sp = getSharedPreferences("PBD", Activity.MODE_PRIVATE);
+            longitude = Double.parseDouble(sp.getString("latitude", null));
+            latitude = Double.parseDouble(sp.getString("longitude", null));
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -167,6 +209,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     public void openCamera(View view) {
+        Toast.makeText(MapsActivity.this, "Open The camera", Toast.LENGTH_SHORT).show();
+
         // create Intent to take a picture and return control to the calling application
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
@@ -175,6 +219,187 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     public void openAnswerForm(View view) {
+        Toast.makeText(MapsActivity.this, "Open the Answer Form", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this,AnswerActivity.class);
+        startActivityForResult(intent, 18);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        String value = "";
+        if (requestCode == 18) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                // The user picked a contact.
+                // The Intent's data Uri identifies which contact was selected.
+
+                // Do something with the contact here (bigger example below)
+                Bundle extras = getIntent().getExtras();
+                if (extras != null) {
+                    value = extras.getString("status");
+                }
+
+                if (value.equals("ok")){
+                    SharedPreferences sp = getSharedPreferences("PBD", Activity.MODE_PRIVATE);
+                    longitude = Double.parseDouble(sp.getString("latitude", null));
+                    latitude = Double.parseDouble(sp.getString("longitude", null));
+
+                    LatLng map = new LatLng(latitude, longitude);
+
+                    answerLocation.remove();
+                    answerLocation = mMap.addMarker(new MarkerOptions().position(map).title("Dimana hayoo?"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(map));
+                    mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+                }
+            }
+        }
+    }
+
+    public class connection extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... args) {
+
+            try {
+                socket = new Socket(ServerIP, ServerPort);
+                JSONObject json = new JSONObject();
+                json.put("com", "req_loc");
+                json.put("nim", "13513005");
+                System.err.println("Connected");
+
+                String date = java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
+                if (firstConnect){
+                    writeToFile("Client:\n" + json + "\n" + date + "\n");
+                } else {
+                    String log = readFromFile();
+                    writeToFile(log + "\nClient:\n" + json + "\n" + date + "\n");
+                }
+
+                PrintWriter out = new PrintWriter(socket.getOutputStream());
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                out.println(json.toString());
+                out.flush();
+                System.err.println(json);
+
+                String res = in.readLine();
+                System.err.println(res);
+                return res;
+
+            } catch (UnknownHostException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally{
+                if(socket != null){
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String response) {
+            try {
+                (Toast.makeText(getApplicationContext(),response,Toast.LENGTH_LONG)).show();
+
+                String date = java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
+                String log = readFromFile();
+                writeToFile(log + "\nServer:\n" + response + "\n" + date + "\n");
+
+                JSONObject jsonResponse = new JSONObject(response);
+                status = jsonResponse.getString("status");
+                latitude = jsonResponse.getDouble("longitude");
+                longitude = jsonResponse.getDouble("latitude");
+                token = jsonResponse.getString("token");
+
+                date = java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
+                log = readFromFile();
+                writeToFile(log + "\nServer:\n" + jsonResponse + "\n" + date + "\n");
+
+                SharedPreferences sp = getSharedPreferences("PBD", Activity.MODE_PRIVATE);
+                SharedPreferences.Editor edit = sp.edit();
+                edit.putString("token",token);
+                edit.commit();
+                firstConnect = false;
+
+                LatLng map = new LatLng(latitude, longitude);
+                answerLocation = mMap.addMarker(new MarkerOptions().position(map).title("Answer Location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(map));
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    public void writeToFile(String data)
+    {
+        String path = Environment.getExternalStorageDirectory() + File.separator  + "PBD";
+        // Create the folder.
+        File folder = new File(path);
+        folder.mkdirs();
+
+        // Create the file.
+        File file = new File(folder, "log.txt");
+
+        // Save your stream, don't forget to flush() it before closing it.
+
+        try
+        {
+            file.createNewFile();
+            FileOutputStream fOut = new FileOutputStream(file);
+            OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
+            myOutWriter.append(data);
+
+            myOutWriter.close();
+
+            fOut.flush();
+            fOut.close();
+        }
+        catch (IOException e)
+        {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+    private String readFromFile() {
+
+        String ret = "";
+
+        String path = Environment.getExternalStorageDirectory() + File.separator  + "PBD";
+
+        //Get the text file
+        File file = new File(path,"log.txt");
+
+        //Read text from file
+        StringBuilder text = new StringBuilder();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
+            br.close();
+        }
+        catch (IOException e) {
+            //You'll need to add proper error handling here
+        }
+
+        ret = text.toString();
+        return ret;
     }
 }
